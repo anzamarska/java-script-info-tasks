605f3fa2e502cb872eccfd716e6b173f92d8c977
anzamarska <anzamarska@gmail.com>
2020-03-20 17:10:41 +0100

    2.9 done

---
+++ 2.10-if-statement.js
@@ -0,0 +1,37 @@
+// 2
+
+const nameJS = prompt("What is the “official” name of JavaScript?", );
+
+
+if (nameJS == "ECMAScript"){
+    alert ("Right!")
+} else {
+    alert ("Didn’t know? ECMAScript!")
+}
+
+
+//3
+
+const number =prompt("Choose number", );
+
+if (number > 0){
+    alert("1")
+} else if (number <0){
+    alert ("-1")
+} else if (number == 0){
+    alert ("0")
+}
+
+
+//3
+
+let result = (a + b < 4) ? 'Below' : 'Over';
+
+//4
+
+let message = 
+(login == 'Employee') ? 'Hello' : 
+(login == 'Director') ? 'Greetings' :
+(login == '') ? 'No login' :
+'';
+
